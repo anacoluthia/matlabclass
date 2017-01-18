07e1665b6be5b03aa779ef16c856af8933fcdd5e
Anacoluthia <loswald@gmail.com>
2017-01-18 16:26:10 -0500

    Add files via upload

---
+++ loswald2.m
@@ -0,0 +1,24 @@
+%Evaluation 2
+
+%Part 1:  
+radii = [7:6:32];
+area = pi.*radii.^2;
+circum = 2*pi.*radii;
+disp('  ');
+disp(['For Part 1, the areas are ' num2str(area)]);
+disp(['and the circumferences are ' num2str(circum)]);
+disp('  ');
+
+%Part 2:
+sqsidecircum = circum./4;
+sqsidearea = sqrt(area);
+disp(['For Part 2, the first squares have side lengths of ' num2str(sqsidecircum)]);
+disp(['and the second squares have side lengths of ' num2str(sqsidearea)]);
+disp('  ');
+
+%Part 3:
+width = sqrt(area./1.61);
+height = 1.61.*width;
+disp(['For Part 3, the widths are ' num2str(width)]);
+disp(['and the heights are ' num2str(height)]);
+disp('  ');
