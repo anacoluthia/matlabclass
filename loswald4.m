de4d94bd91e0d4551c67a2e379fea9c24d213dc0
loswald <loswald@CHEMD06C.ts.odu.edu>
2017-02-08 13:11:53 -0500

    Week 4 Final version

--- loswald4.m
+++ loswald4.m
@@ -4,8 +4,6 @@ year = data(:,1);
 month = data(:,2);
 time = year + month/12;
 temp = data(:,3);
-% meantemp = mean(temp);
-% anomaly = temp - meantemp;
 % Part 1:  
 ix = find(month == 2);
 ig = find(month == 8);
@@ -20,46 +18,65 @@ zA = ones(1,length(Aug));
 avgF = zF*FebAvg;
 avgA = zA*AugAvg;
 Diff = AugAvg - FebAvg;
-zd = ones(1,length(time));
-zd1 = zd * Diff;
+% zd = ones(1,length(time));
+% zd1 = zd * Diff;
 figure(1);
 plot(timeF, Feb, timeA, Aug);
 hold on 
 plot(timeF, avgF, timeA, avgA);
-plot(time, zd1);
+% plot(time, zd1);
 xlabel('Month');
-ylabel('Temp, degrees F')
-title('February & December');
+ylabel('Temp,°F') % alt+248 inserts degree symbol
+title('February & August');
+% It was STUPIDLY hard to figure out how to parse this - I still don't
+% understand what %s does, or why the examples had %s followed by %d;
+% changing %s to %d in the below causes the text to repeat
+Diffstr = sprintf('*Avg difference btwn Feb & Aug is %s °F', num2str(Diff,'%2.2f'));
+text(1945, 63, Diffstr);
+legend('February', 'August', 'Location', 'Best');
 hold off
 
 % Part 2:  
  for i = 1:12;
      m = find(month == i);
-     meantemp(i) = mean(temp(m));
+     meantemp(i) = mean(temp(m)); % meantemp is monthly average across 
+     % all years
  end
 mon = 1:12;
 figure(2);
 plot(mon, meantemp);
 hold on
 xlim([0.5 12.5]);
-xlabel('Month');
-ylabel('Temp, degrees F')
-title('Monthly Average Temps');
+xlabel('Month'); % Any easy way to convert to Jan, Feb, ... ?
+ylabel('Temp, °F')
+title('Monthly Average Temp, 1946 - 2010');
 maxT = max(meantemp);
 minT = min(meantemp);
 maxTT = find(meantemp == maxT);
 minTT = find(meantemp == minT);
-plot(maxTT, maxT, 'r*','MarkerSize',14);
-plot(minTT, minT, 'b*','MarkerSize',14);
+plot(maxTT, maxT, 'r*','MarkerSize',13);
+plot(minTT, minT, 'b*','MarkerSize',13);
+legend('Average','Warmest, July','Coldest, January', 'Location', 'NorthWest')
 hold off
 
-for j = 1:12;
-    m1 = find(month == j);
-    anom = temp(m)- meantemp(j);
-    mon_anom(j) = mean(abs(anom));
+% Part 3:
+for i = 1:12;
+    m = find(month == i);
+    anomaly = temp(m)- meantemp(i); % diff btwn that yr & all yrs
+    mon_anom(i) = mean(abs(anomaly)); % monthly mean abs diff
 end 
 figure(3);
 plot(mon, mon_anom);
-    
-
+hold on 
+xlabel('Month');
+ylabel('Mean Monthly Absolute Temp Anomaly, °F');
+title('Norfolk Mean Absolute Monthly Anomaly over 65 Years');
+maxAnom = max(mon_anom);
+minAnom = min(mon_anom);
+maxAT = find(mon_anom == maxAnom);
+minAT = find(mon_anom == minAnom);
+plot(maxAT, maxAnom, 'r*','MarkerSize',13);
+plot(minAT, minAnom, 'b*','MarkerSize',13);
+legend('Abs Anomaly','Most variability','Least variability')
+hold off
  
