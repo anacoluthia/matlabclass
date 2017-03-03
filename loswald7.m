49abb7a692b532ad7164b8c37cb017349e209083
Anacoluthia <loswald@gmail.com>
2017-03-03 11:53:30 -0500

    Add files via upload

--- loswald7.m
+++ loswald7.m
@@ -12,10 +12,13 @@ cxy=corr(M);  % cxy is a 3x3 matrix
 figure(1);
 plot(t, ob1, t, ob2, t, ob3);
 hold on 
-legend('Obs 1', 'Obs 2', 'Obs 3');
-text(275,12, 'Col 1 to col 2 corr = -0.476');
-text(275,11, 'Col 1 to col 3 corr = -0.724');
-text(275,10, 'Col 2 to col 3 corr = -0.172');
+legend('Obs 1', 'Obs 2', 'Obs 3', 'location', 'NorthWest');
+legend boxoff;
+xlabel('Days');
+ylabel('Unspecified units');
+text(275,12, '1-2 corr = -0.476');
+text(275,11, '1-3 corr = -0.724');
+text(275,10, '2-3 corr = -0.172');
 hold off
 
 % Part 2: 
@@ -40,7 +43,8 @@ hold on
 title('E7 data 1&2');
 xlabel('Time, days');
 ylabel('Units not specified')
-legend('Col 2 data', 'Col 3 data');
+legend('Obs 1 data', 'Obs 2 data');
+legend boxoff;
 hold off
 subplot(2,1,2)
 plot(lag*dt,cAB)
@@ -82,11 +86,10 @@ for i = 1:3;
         subplot(4,1,k);
         plot(lag, cAB);
         hold on 
-        legend(strcat('Lag',{' '},num2str([i,j],' Obs %d')), 'Location', 'BestOutside');
-        % In this case, setting legend outside of the plot was the only way
-        % to avoid it sitting over data
+        title(strcat('Lag - ',{' '},num2str([i,j],' Obs %d')));
+        text(10,0.6,strcat('Lag =',{' '},num2str(maxLd(k),'%d days')));
         k = k+1;
-       
+        hold off
     end
 end
 
@@ -95,5 +98,4 @@ plot(t,ob1, t, ob2, t, ob3);
 xlim([0 370]); 
 title('Original data');
 xlabel('Days');
-hold off
-disp(['Lag for corr 1 is ', num2str(maxLd(1, 1)]);
+
